260548e473b230ef81f0daf9fa6023225ab857aa
Vlad <175618876+v3xro@users.noreply.github.com>
2024-07-15 21:17:39 +0100

    exercises up to quiz1

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,7 +10,13 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(n: u32) -> u32 {
+    if n > 40 {
+        n
+    } else {
+        n * 2
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
